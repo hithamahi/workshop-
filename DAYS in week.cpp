274c0b9d34a7c3ba315e1380afd20ec6dea22391
hithamahi <96760980+hithamahi@users.noreply.github.com>
2021-12-28 11:44:42 +0530

    Add files via upload

---
+++ DAYS in week.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int day;
+	switch(day)
+	{
+		case 1 :
+			day =1;
+			cout<<" MONDAY "<<day<<endl;
+			break;
+		case 2 :	
+			day =2;
+			cout<<" TUESDAY "<<day<<endl;
+			break;
+		case 3 :	
+			day =3;
+			cout<<" WEDNESDAY "<<day<<endl;
+			break;
+		case 4 :
+		    day =4;
+			cout<<" THURSDAY "<<day<<endl;
+			break;
+		case 5 :
+			day =5;
+			cout<<" FRIDAY "<<day<<endl;
+			break;
+		case 6 :
+			day =6;
+			cout<<" SATURDAY "<<day<<endl;
+			break;
+		case 7 :
+		    day =7;
+			cout<<" SUNDAY "<<day<<endl;
+			break;		
+	}
+}
